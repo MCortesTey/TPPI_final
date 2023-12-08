105c90a169615e7409a55100fe10cd9dfec26d22
belupetri <belupetri01@gmail.com>
2023-12-08 18:17:55 -0300

    Revert "Revert "add query4""
    This reverts commit 159a96f84467425d67318ebf98c970f128a35aaf.

--- bikesADT.h
+++ bikesADT.h
@@ -13,7 +13,7 @@ typedef struct query1 {
     size_t cantMem; // cantidad de viajes empezados por miembros
     size_t cantCas; // cantidad de viajes empezados por casuales
     size_t cantTot; // cantidad de viajes empezados totales
-    List1 tail;
+    struct query1 * tail;
 } Tquery1;
 
 
@@ -34,10 +34,10 @@ typedef struct query2 {
     char * nameSt; // nombre de la estacion de inicio
     char * nameEnd; // nombre de la esatcion de fin
     time_t oldestTrip; // fecha y hora del viaje mas antiguo
-    List2 tail;
+    struct query2 *tail;
 } Tquery2;
 
-typedef Tquery2
+typedef Tquery2 * List2;
 
 
 typedef struct query4{ 
@@ -58,7 +58,7 @@ bikeRentalSystemADT newBikeRentalSystem(int minYear, int maxYear);
 int addStation(bikeRentalSystemADT bikeRentalSystem, char * name, int id);
 
 //registra un viaje y devuelve 1 si se agrego con exito.
-int addTrip(bikeRentalSystemADT bikeRentalSystem, int startId, int endId, int day, int month, int year, int isMember);
+int addTrip(bikeRentalSystemADT bikeRentalSystem, int startId, int endId, int minutes, int hour, int day, int month, int year, int isMember);
 
 // apunta al primer elemento
 void toBegin (bikeRentalSystemADT bikeRentalSystem);
@@ -81,8 +81,13 @@ size_t getMemTrips ( bikeRentalSystemADT bikeRentalSystem );
 // retorna indice correspondiente a la estacion donde iter apunta, en la matriz de trips
 size_t getIdx ( bikeRentalSystemADT bikeRentalSystem );
 
-List1 query1 ( bikeRentalSystemADT bikeRentalSystem );
+List1 query1( bikeRentalSystemADT bikeRentalSystem );
 
-List1 query2 ( bikeRentalSystem bikeRentalSystem )
+List2 query2( bikeRentalSystemADT bikeRentalSystem );
 
+
+Tquery3 query3(bikeRentalSystemADT bikeRentalSystem  );
+
+
+List4 query4( bikeRentalSystemADT bikeRentalSystem);
 #endif
